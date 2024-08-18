977bd72629e1c99504914c2849de6593b74d7efd
SilverShadow123 <hasanmohasin852@gmail.com>
2024-08-18 19:19:05 +0600

    learning

--- lib/main.dart
+++ lib/main.dart
@@ -12,87 +12,26 @@ class nav_the extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Home(),
+      title: 'dragon',
+      color: Colors.blue,
+      theme: ThemeData.light(),
+
     );
   }
 }
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => Home1(Image.asset('assets/images/a-girl.jpg'))));
-                },
-                child: Text('Go Home 1')),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => Home2('The quick brown fox jumps')));
-                },
-                child: Text('Go Home 2')),
-          ],
-        ),
-      ),
-    );
-  }
+  State<Home> createState() => _HomeState();
 }
 
-class Home1 extends StatelessWidget {
-
-   var msg;
-
-   Home1(
-
-      this.msg,
-
-      {super.key});
-
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home1'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                  context, MaterialPageRoute(builder: (context) => Home2('The quick brown fox jumps')));
-            },
-            child: msg),
 
-      ),
-    );
-  }
-}
-
-class Home2 extends StatelessWidget {
-
-  String msg;
-
-  Home2(
-
-  this.msg,
-
-      {super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(msg),
-      ),
     );
   }
 }
