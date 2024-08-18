2216ef805b00b596555f2b0186d87ea36d50a075
SilverShadow123 <hasanmohasin852@gmail.com>
2024-08-18 22:32:02 +0600

    learning

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:device_preview/device_preview.dart';
 
 void main() {
-  runApp(nav_the());
+  runApp(
+      DevicePreview(enabled: !kReleaseMode, builder: (context) => nav_the()));
 }
 
 class nav_the extends StatelessWidget {
@@ -11,6 +14,9 @@ class nav_the extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       home: Home(),
       title: 'dragon',
       color: Colors.blue,
@@ -29,147 +35,21 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+    var height = MediaQuery.of(context).size.height;
+    var Orientation = MediaQuery.of(context).orientation;
+
     return Scaffold(
-      //Aspcet Ratio
-
-      // body: Container(
-      //   width: double.infinity,
-      //   height: double.infinity,
-      //   color: Colors.red,
-      //   alignment: Alignment.center,
-      //   child: AspectRatio(
-      //     aspectRatio: 16 / 9,
-      //     child: Container(
-      //       color: Colors.blue,
-      //     ),
-      //   ),
-      // ),
-
-      //Factionally sized box
-
-      // body: Center(
-      //   child: FractionallySizedBox(
-      //     widthFactor: 0.5,
-      //     heightFactor: 0.3,
-      //     child: Container(
-      //       color: Colors.blue,
-      //     ),
-      //   ),
-      // ),
-
-      //layout builder
-
-      // body: Center(
-      //   child: LayoutBuilder(builder: (BuildContext context,BoxConstraints constraints){
-      //     if (constraints.maxWidth>600)
-      //       {
-      //         return Container(height: 200,width: 200,color: Colors.blue,);
-      //       }
-      //     else{
-      //       return Container(height: 300,width: 300,color: Colors.red,);
-      //     }
-      //   }),
-      // ),
-
-      //Scroll view
-
-      // body: SingleChildScrollView(
-      //   scrollDirection: Axis.horizontal,
-      //   child: Center(
-      //     child: Row(
-      //       children: [
-      //         Container(
-      //           height: 200,
-      //           width: 200,
-      //           color: Colors.blue,
-      //         ),Container(
-      //           height: 200,
-      //           width: 200,
-      //           color: Colors.red,
-      //         ),Container(
-      //           height: 200,
-      //           width: 200,
-      //           color: Colors.yellow,
-      //         ),Container(
-      //           height: 200,
-      //           width: 200,
-      //           color: Colors.green,
-      //         ),Container(
-      //           height: 200,
-      //           width: 200,
-      //           color: Colors.orange,
-      //         ),Container(
-      //           height: 200,
-      //           width: 200,
-      //           color: Colors.purple,
-      //         ),Container(
-      //           height: 200,
-      //           width: 200,
-      //           color: Colors.pink,
-      //         ),Container(
-      //           height: 200,
-      //           width: 200,
-      //           color: Colors.grey,
-      //         ),
-      //       ],
-      //     ),
-      //   ),
-      // ),
-
-      //
-
-      // body: Column(
-      //   children: [
-      //     Expanded(
-      //       flex: 2,
-      //       child: Container(
-      //         color: Colors.blue,
-      //       ),
-      //     ),
-      //     Expanded(
-      //       child: Container(
-      //         color: Colors.red,
-      //       ),
-      //     ),
-      //     Expanded(
-      //       flex: 3,
-      //       child: Container(
-      //         color: Colors.amber,
-      //       ),
-      //     )
-      //   ],
-      // ),
-
-      //flexible
-
-      // body: Column(
-      //   children: [
-      //     Flexible(
-      //       fit: FlexFit.loose,
-      //       flex: 2,
-      //       child: Container(
-      //         color: Colors.blue,
-      //       ),
-      //     ),
-      //     Flexible(
-      //
-      //       child: Container(
-      //         color: Colors.red,
-      //       ),
-      //     ),
-      //     Flexible(
-      //       fit: FlexFit.loose,
-      //       flex: 3,
-      //       child: Container(
-      //         color: Colors.amber,
-      //       ),
-      //     )
-      //   ],
-      // ),
-
-
-
-
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('width ${width}'),
+            Text('height ${height}'),
+            Text(' ${Orientation}'),
+          ],
+        ),
+      ),
     );
   }
 }
