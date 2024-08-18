1d461adc7c303fcdcb97a38bc779c5df2bb3935b
SilverShadow123 <hasanmohasin852@gmail.com>
2024-08-18 21:17:25 +0600

    learning

--- lib/main.dart
+++ lib/main.dart
@@ -45,17 +45,131 @@ class _HomeState extends State<Home> {
       //   ),
       // ),
 
-      //Sized factor
+      //Factionally sized box
 
-      body: Center(
-        child: FractionallySizedBox(
-          widthFactor: 0.5,
-          heightFactor: 0.3,
-          child: Container(
-            color: Colors.blue,
-          ),
-        ),
-      ),
+      // body: Center(
+      //   child: FractionallySizedBox(
+      //     widthFactor: 0.5,
+      //     heightFactor: 0.3,
+      //     child: Container(
+      //       color: Colors.blue,
+      //     ),
+      //   ),
+      // ),
+
+      //layout builder
+
+      // body: Center(
+      //   child: LayoutBuilder(builder: (BuildContext context,BoxConstraints constraints){
+      //     if (constraints.maxWidth>600)
+      //       {
+      //         return Container(height: 200,width: 200,color: Colors.blue,);
+      //       }
+      //     else{
+      //       return Container(height: 300,width: 300,color: Colors.red,);
+      //     }
+      //   }),
+      // ),
+
+      //Scroll view
+
+      // body: SingleChildScrollView(
+      //   scrollDirection: Axis.horizontal,
+      //   child: Center(
+      //     child: Row(
+      //       children: [
+      //         Container(
+      //           height: 200,
+      //           width: 200,
+      //           color: Colors.blue,
+      //         ),Container(
+      //           height: 200,
+      //           width: 200,
+      //           color: Colors.red,
+      //         ),Container(
+      //           height: 200,
+      //           width: 200,
+      //           color: Colors.yellow,
+      //         ),Container(
+      //           height: 200,
+      //           width: 200,
+      //           color: Colors.green,
+      //         ),Container(
+      //           height: 200,
+      //           width: 200,
+      //           color: Colors.orange,
+      //         ),Container(
+      //           height: 200,
+      //           width: 200,
+      //           color: Colors.purple,
+      //         ),Container(
+      //           height: 200,
+      //           width: 200,
+      //           color: Colors.pink,
+      //         ),Container(
+      //           height: 200,
+      //           width: 200,
+      //           color: Colors.grey,
+      //         ),
+      //       ],
+      //     ),
+      //   ),
+      // ),
+
+      //
+
+      // body: Column(
+      //   children: [
+      //     Expanded(
+      //       flex: 2,
+      //       child: Container(
+      //         color: Colors.blue,
+      //       ),
+      //     ),
+      //     Expanded(
+      //       child: Container(
+      //         color: Colors.red,
+      //       ),
+      //     ),
+      //     Expanded(
+      //       flex: 3,
+      //       child: Container(
+      //         color: Colors.amber,
+      //       ),
+      //     )
+      //   ],
+      // ),
+
+      //flexible
+
+      // body: Column(
+      //   children: [
+      //     Flexible(
+      //       fit: FlexFit.loose,
+      //       flex: 2,
+      //       child: Container(
+      //         color: Colors.blue,
+      //       ),
+      //     ),
+      //     Flexible(
+      //
+      //       child: Container(
+      //         color: Colors.red,
+      //       ),
+      //     ),
+      //     Flexible(
+      //       fit: FlexFit.loose,
+      //       flex: 3,
+      //       child: Container(
+      //         color: Colors.amber,
+      //       ),
+      //     )
+      //   ],
+      // ),
+
+
+
+
     );
   }
 }
